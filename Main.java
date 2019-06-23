bee801d61a492576a06c417d969a1da2776d5c97
= <=>
2019-06-23 15:27:28 +0700

    bug fix

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -33,8 +28,9 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         // TODO code application logic here
+
         Main m = new Main();
 
         if (args.length == 1) {
@@ -56,8 +52,6 @@ public class Main {
         InetAddress inetAddress = InetAddress.getLocalHost();
         switch (type) {
             case "client":
-                System.out.println("IP Address:- " + getCurrentEnvironmentNetworkIp());
-                System.out.println("Host Name:- " + inetAddress.getHostName());
                 client();
                 break;
             case "server":
